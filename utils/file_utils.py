6fb55661df968c7fc3d0401d22cf688361bdc522
RandolphPeralta <ran.dol1995@hotmail.com>
2025-06-08 23:33:52 -0500

    changes
    conexiones_rpa.py

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -31,3 +31,20 @@ def guardar_control_entregas_json(numero_autorizacion, datos):
     
     print(f"✅ Datos de control de entregas guardados en '{ruta}'")
     return str(ruta)
+
+"""
+def guardar_json(numero_autorizacion, datos):
+    if carpeta == autorizaciones:
+        carpeta = Path("data/autorizaciones")
+    else carpeta == control_entregas :
+        carpeta = Path("data/control_entregas")
+     
+    carpeta.mkdir(parents=True, exist_ok=True)
+
+    ruta = carpeta / f'documento_{numero_autorizacion}.json'
+    with open(ruta, 'w', encoding='utf-8') as f:
+        json.dump(datos, f, ensure_ascii=False, indent=4)
+    
+    print(f"✅ Datos de control de entregas guardados en '{ruta}'")
+    return str(ruta)
+"""
